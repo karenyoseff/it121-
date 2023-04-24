34afca4886cf1e9b9627f5e68ae1476d8913a453
Karen Yoseff <52435963+karenyoseff@users.noreply.github.com>
2023-04-23 18:30:24 -0700

    Add files via upload

---
+++ assg2/assg2.js
@@ -0,0 +1,11 @@
+let str1 = 'Laurence'; 
+let str2 = "Svekis"; 
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+console.log(typeof str1); // output: string
+console.log(typeof str2); // output: string
+console.log(typeof val1); // output: undefined
+console.log(typeof val2); // output: object
+console.log(typeof myNum); // output: number
